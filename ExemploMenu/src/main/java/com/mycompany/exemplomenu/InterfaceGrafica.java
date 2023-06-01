9e00b44032cd3dedfc5854541c507f10cdb13ee3
Lucas Pessoli <pessolilucas@gmail.com>
2023-06-01 19:17:31 -0300

    FIX inserção IMPLEMENT bolha, mostrarVetorOriginal
    Deixado rascunho de vetorOriginal caso necessário no futuro

--- ExemploMenu/src/main/java/com/mycompany/exemplomenu/InterfaceGrafica.java
+++ ExemploMenu/src/main/java/com/mycompany/exemplomenu/InterfaceGrafica.java
@@ -2,9 +2,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.testeee;
+package com.mycompany.exemplomenu;
 
-import java.util.ArrayList;
 import javax.swing.JOptionPane;
 
 /**
@@ -17,21 +16,65 @@ public class InterfaceGrafica {
         JOptionPane.showMessageDialog(null, msg);
     }
     
-    public ArrayList<Integer> insercao(int valor){
-        ArrayList<Integer> lista = new ArrayList<>();
+//    public int[] vetorOriginal(ArrayList<Integer> lista ){
+//        int[] vetorOrg = new int[lista.size()];
+//        for(int i =0; i<lista.size();i++){
+//            vetorOrg[i] = lista.get(i);
+//        }
+//        return vetorOrg;
+//    }
+    
+    public void mostrarVetorOriginal(int valor, int[] vet){
+        String original = "";
         for(int i=0;i<valor;i++){
-            int numero = Integer.parseInt(JOptionPane.showInputDialog("Informe o número da fila: [" + (i+1) + "]"));
-            lista.add(numero);
+            original = original + vet[i] + ", ";
         }
-        return lista;
+        messager("Seu vetor original é: " + original);
     }
     
-//    public ArrayList<Integer> selecao(int valor){
-//        ArrayList<Integer> lista = new ArrayList<>();
-//        for(int i=0;i<valor;i++){
-//            int numero = Integer.parseInt(JOptionPane.showInputDialog("Informe o número da fila: [" + (i+1) + "]"));
-//            lista.add(numero);
-//        }
-//        return lista;
-//    }
+    public int[] insercao(int valor) {
+        int[] vetor = new int[valor];
+        for (int i = 0; i < valor; i++) {
+            int numero = Integer.parseInt(JOptionPane.showInputDialog("Informe o número da fila: [" + (i + 1) + "]"));
+            vetor[i] = numero;
+        }
+        mostrarVetorOriginal(valor, vetor);
+        for (int i = 1; i < valor; i++) {
+            int chave = vetor[i];
+            int j = i - 1;
+            while (j >= 0 && vetor[j] > chave) {
+                vetor[j+1] = vetor[j];
+                j--;
+            }
+            vetor[j + 1] = chave;
+        }
+        return vetor;
+    }
+    
+    public int[] selecao(int valor){
+        int[] vetor = new int[valor];
+        for(int i=0;i<valor;i++){
+            int numero = Integer.parseInt(JOptionPane.showInputDialog("Informe o número da fila: [" + (i+1) + "]"));
+        }
+        return vetor;
+    }
+    
+    public int[] bolha(int valor) {
+        int[] vetor = new int[valor];
+        for (int i = 0; i < valor; i++) {
+            int numero = Integer.parseInt(JOptionPane.showInputDialog("Informe o número da fila: [" + (i + 1) + "]"));
+            vetor[i] = numero;
+        }
+        mostrarVetorOriginal(valor, vetor);
+        for (int i = 0; i < valor - 1; i++) {
+            for (int j = 0; j < valor - 1 - i; j++) {
+                if (vetor[j] > vetor[j + 1]) {
+                    int tp = vetor[j];
+                    vetor[j] = vetor[j + 1];
+                    vetor[j+1] = tp;
+                }
+            }
+        }
+        return vetor;
+    }
 }
