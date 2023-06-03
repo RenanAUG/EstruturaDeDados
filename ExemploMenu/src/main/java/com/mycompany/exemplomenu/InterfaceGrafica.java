23ed425478958055ebc78adae92a2db90d44053b
Lucas Pessoli <pessolilucas@gmail.com>
2023-06-02 23:01:04 -0300

    FIX referencia da package

--- ExemploMenu/src/main/java/com/mycompany/exemplomenu/InterfaceGrafica.java
+++ ExemploMenu/src/main/java/com/mycompany/exemplomenu/InterfaceGrafica.java
@@ -1,8 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.menu;
+package com.mycompany.exemplomenu;
 
 import javax.swing.JOptionPane;
 
