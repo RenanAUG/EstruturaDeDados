331d1516c36552b3f07a296badb52ef413596a87
Renan Augusto <114965499+RenanAUG@users.noreply.github.com>
2023-06-02 22:42:51 -0300

    Alterações nas Classes
    Finalização da classe vetor Seleção e adicionando o tempo de Execução nos métodos

--- ExemploMenu/src/main/java/com/mycompany/exemplomenu/InterfaceGrafica.java
+++ ExemploMenu/src/main/java/com/mycompany/exemplomenu/InterfaceGrafica.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.exemplomenu;
+package com.mycompany.menu;
 
 import javax.swing.JOptionPane;
 
@@ -11,11 +11,11 @@ import javax.swing.JOptionPane;
  * @author lucas
  */
 public class InterfaceGrafica {
-    
-    public void messager(String msg){
+
+    public void messager(String msg) {
         JOptionPane.showMessageDialog(null, msg);
     }
-    
+
 //    public int[] vetorOriginal(ArrayList<Integer> lista ){
 //        int[] vetorOrg = new int[lista.size()];
 //        for(int i =0; i<lista.size();i++){
@@ -23,15 +23,14 @@ public class InterfaceGrafica {
 //        }
 //        return vetorOrg;
 //    }
-    
-    public void mostrarVetorOriginal(int valor, int[] vet){
+    public void mostrarVetorOriginal(int valor, int[] vet) {
         String original = "";
-        for(int i=0;i<valor;i++){
+        for (int i = 0; i < valor; i++) {
             original = original + vet[i] + ", ";
         }
         messager("Seu vetor original é: " + original);
     }
-    
+
     public int[] insercao(int valor) {
         int[] vetor = new int[valor];
         for (int i = 0; i < valor; i++) {
@@ -43,22 +42,41 @@ public class InterfaceGrafica {
             int chave = vetor[i];
             int j = i - 1;
             while (j >= 0 && vetor[j] > chave) {
-                vetor[j+1] = vetor[j];
+                vetor[j + 1] = vetor[j];
                 j--;
             }
             vetor[j + 1] = chave;
         }
         return vetor;
     }
-    
-    public int[] selecao(int valor){
+
+    public int[] selecao(int valor) {
         int[] vetor = new int[valor];
-        for(int i=0;i<valor;i++){
-            int numero = Integer.parseInt(JOptionPane.showInputDialog("Informe o número da fila: [" + (i+1) + "]"));
+        for (int i = 0; i < valor; i++) {
+            int numero = Integer.parseInt(JOptionPane.showInputDialog("Informe o número da fila: [" + (i + 1) + "]"));
+            vetor[i] = numero;
         }
+
+        mostrarVetorOriginal(valor, vetor);
+        for (int i = 0; i < valor - 1; i++) {
+            int posicao = i;
+
+            for (int j = i + 1; j < valor; j++) {
+                if (vetor[j] < vetor[posicao]) {
+                    posicao = j;
+                }
+            }
+
+            if (posicao != i) {
+                int aux = vetor[i];
+                vetor[i] = vetor[posicao];
+                vetor[posicao] = aux;
+            }
+        }
+
         return vetor;
     }
-    
+
     public int[] bolha(int valor) {
         int[] vetor = new int[valor];
         for (int i = 0; i < valor; i++) {
@@ -71,7 +89,7 @@ public class InterfaceGrafica {
                 if (vetor[j] > vetor[j + 1]) {
                     int tp = vetor[j];
                     vetor[j] = vetor[j + 1];
-                    vetor[j+1] = tp;
+                    vetor[j + 1] = tp;
                 }
             }
         }
---
+++ ExemploMenu/src/main/java/com/mycompany/exemplomenu/Menu.java
@@ -0,0 +1,93 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.menu;
+
+import java.util.Currency;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author aluno
+ */
+public class Menu {
+
+    public static void main(String[] args) {
+        String resultado = "";
+        InterfaceGrafica painel = new InterfaceGrafica();
+        int valores;
+        long inicio = System.currentTimeMillis();
+        long fim = System.currentTimeMillis();
+        long tempoExecucao = fim - inicio;
+
+        int opcao = 99;
+        while (!(opcao == 0)) {
+            opcao = Integer.parseInt(JOptionPane.showInputDialog(
+                    "1 - Vetor de Inserção\n"
+                    + "2 - Vetor de Seleção\n"
+                    + "3 - Vetor de bolha\n"
+                    + "0 - Sair"));
+            switch (opcao) {
+                case 1:
+                    valores = Integer.parseInt(JOptionPane.showInputDialog("Informe quantos valores serão adicionados na lista:"));
+                    int[] vetor = new int[valores];
+                    inicio = System.currentTimeMillis();
+                    
+                    vetor = painel.insercao(valores);
+                    for (Integer item : vetor) {
+                        resultado = resultado + item.toString() + ", ";
+                    }
+                    
+                    fim = System.currentTimeMillis();
+                    tempoExecucao = fim - inicio;
+                    
+                    painel.messager("A ordem do vetor inserção foi de: " + resultado +
+                                    "\n Tempo Execução: " + tempoExecucao + "milissegundos");
+                    break;
+
+                case 2:
+                    valores = Integer.parseInt(JOptionPane.showInputDialog("Informe quantos valores serão adicionados na lista:"));
+                    int[] vetorSelecao = new int[valores];
+                    inicio = System.currentTimeMillis();
+                    vetorSelecao = painel.selecao(valores);
+                    for (Integer item : vetorSelecao) {
+                        resultado = resultado + item.toString() + ", ";
+                    }
+
+                    fim = System.currentTimeMillis();
+                    tempoExecucao = fim - inicio;
+                    
+                    painel.messager("A ordem do vetor Seleção foi de: " + resultado +
+                                    "\n Tempo Execução: " + tempoExecucao + "milissegundos");
+                    break;
+
+                case 3:
+                    valores = Integer.parseInt(JOptionPane.showInputDialog("Informe quantos valores serão adicionados na lista:"));
+                    int[] vetorBolha = new int[valores];
+                    
+                    inicio = System.currentTimeMillis();
+                    
+                    vetorBolha = painel.bolha(valores);
+                    for (Integer item : vetorBolha) {
+                        resultado = resultado + item.toString() + ", ";
+                    }
+                    
+                    fim = System.currentTimeMillis();
+                    tempoExecucao = fim - inicio;
+                    
+                    painel.messager("A ordem do vetor bolha foi de: " + resultado +
+                                    "\n Tempo Exeucação: " + tempoExecucao + "milissegundos");
+                    break;
+
+                default:
+                    if (opcao != 0) {
+                        System.out.println("Opção inválida");
+                    } else {
+                        System.out.println("Saindo...");
+                    }
+                    System.exit(0);
+                    break;
+            }
+        }
+    }
+}
