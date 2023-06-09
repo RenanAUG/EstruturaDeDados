1bae6e44193c08192778d9e7a2d59d4dd5a23336
Renan Augusto <114965499+RenanAUG@users.noreply.github.com>
2023-06-09 16:42:10 -0300

    FIX ExemploMenu
    O tempo de Execução estava aparecendo antes do exibir o vetor ordenado

--- ExemploMenu/src/main/java/com/mycompany/exemplomenu/ExemploMenu.java
+++ ExemploMenu/src/main/java/com/mycompany/exemplomenu/ExemploMenu.java
@@ -12,6 +12,7 @@ public class ExemploMenu {
     public static void main(String[] args) {
         String resultado = "";
         String pesquisa = "";
+        String tipo = "";
         InterfaceGrafica painel = new InterfaceGrafica();
         int valores;
 
@@ -25,26 +26,30 @@ public class ExemploMenu {
             switch (opcao) {
                 case 1:
                     valores = Integer.parseInt(JOptionPane.showInputDialog("Informe quantos valores serão adicionados na lista:"));
+                    tipo = "Inserção";
                     int[] vetor = new int[valores];
                     vetor = painel.insercao(valores);
                     for (Integer item : vetor) {
                         resultado = resultado + item.toString() + "\n";
                     }
-                    painel.messager("o vetor odenado é:\n"+resultado);
+                    
+                    painel.mostrarTempoExecucao(tipo, resultado);
                     
                     pesquisa = painel.pedirPesquisa();
                     painel.mostraPesquisa(pesquisa, vetor);
                     break;
 
                 case 2:
-                    valores = Integer.parseInt(JOptionPane.showInputDialog("Informe quantos valores serão adicionados na lista:"));
+                    valores = Integer.parseInt(JOptionPane.showInputDialog("Informe quantos valores serão adicionados na lista:"));        
+                    tipo = "Seleção";
                     int[] vetorSelecao = new int[valores];
                     vetorSelecao = painel.selecao(valores);
 
                     for (Integer item : vetorSelecao) {
                         resultado = resultado + item.toString() + "\n";
                     }
-                    painel.messager("o vetor odenado é:\n"+resultado);
+                    
+                    painel.mostrarTempoExecucao(tipo,resultado);
                     
                     pesquisa = painel.pedirPesquisa();
                     painel.mostraPesquisa(pesquisa, vetorSelecao);
@@ -53,13 +58,15 @@ public class ExemploMenu {
 
                 case 3:
                     valores = Integer.parseInt(JOptionPane.showInputDialog("Informe quantos valores serão adicionados na lista:"));
+                    tipo = "Bolha";
                     int[] vetorBolha = new int[valores];
                     vetorBolha = painel.selecao(valores);
                     
                     for (Integer item : vetorBolha) {
                         resultado = resultado + item.toString() + "\n";
                     }
-                    painel.messager("o vetor odenado é:\n"+resultado);
+                                                            
+                    painel.mostrarTempoExecucao(tipo, resultado);
                     
                     pesquisa = painel.pedirPesquisa();
                     painel.mostraPesquisa(pesquisa, vetorBolha);
